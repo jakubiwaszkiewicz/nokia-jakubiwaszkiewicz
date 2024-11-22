96f782c85900ec06d0d21e1346f636babfde98e8
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2024-11-22 15:21:40 +0100

    fix: reshape input data for predictions and update response format

--- app.py
+++ app.py
@@ -27,31 +27,31 @@ def home():
 }</pre>
         <li>You will receive a JSON response with the model's prediction.</li>
     </ol>
-    <p>Example using <strong>curl</strong>:</p>
+    <p>Checking the prediction using <strong>curl</strong>:</p>
     <span>curl -X POST https://nokia-jakubiwaszkeiwicz-recruitment-task.azurewebsites.net/knn -H "Content-Type: application/json" -d '{"features": [5.0, 13.0, 9.0, 1.0, 0.0, 0.0, 0.0, 13.0, 15.0, 10.0, 15.0, 5.0, 3.0, 15.0, 2.0, 0.0, 11.0, 8.0, 4.0, 12.0, 0.0, 0.0, 8.0, 8.0, 5.0, 8.0, 0.0, 0.0, 9.0, 8.0, 4.0, 11.0, 0.0, 1.0, 12.0, 7.0, 2.0, 14.0, 5.0, 10.0, 12.0, 0.0, 6.0, 13.0, 10.0, 0.0, 0.0, 0.0]}'</span>
     """
 
 @app.route("/knn", methods=["POST"])
 def knn():
     data = request.get_json()
-    input_data = np.array(data["features"])
+    input_data = np.array(data["features"]).reshape(1, -1)
 
     knn_pred = knn_model.predict(input_data)
 
     response = {
-        "Prediction": knn_pred
+        "Prediction": knn_pred.tolist()[0]
     }
     return jsonify(response)
 
 @app.route("/gnb", methods=["POST"])
 def gnb():
     data = request.get_json()
-    input_data = np.array(data["features"])
+    input_data = np.array(data["features"]).reshape(1, -1)
 
     gnb_pred = gnb_model.predict(input_data)
 
     response = {
-        "Prediction": gnb_pred
+        "Prediction": gnb_pred.tolist()[0]
     }
     return jsonify(response)
 
@@ -63,7 +63,7 @@ def mlp():
     mlp_pred = mlp_model.predict(input_data)
 
     response = {
-        "Prediction": mlp_pred
+        "Prediction": mlp_pred.tolist()[0]
     }
     return jsonify(response)
 
