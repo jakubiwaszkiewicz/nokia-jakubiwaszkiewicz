7c66fb98027b1a22dd05f910d200c01f03baa5be
Jakub Iwaszkiewicz <102925580+jakubiwaszkiewicz@users.noreply.github.com>
2024-11-22 15:30:47 +0100

    docs: update home route content and enhance usage instructions

--- app.py
+++ app.py
@@ -11,21 +11,25 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return """
-    <h1>Welcome to the ML Prediction API!</h1>
-    <p>This API allows you to get predictions from three machine learning models: KNN, Gaussian Naive Bayes (GNB), and Multi-Layer Perceptron (MLP).</p>
+    <h1>Hello there!</h1>
+    <p>This server allows you to get predictions from three machine learning models: kNN, Gaussian Naive Bayes (GNB), and Multi-Layer Perceptron (MLP).</p>
+    <p>This is the part of task for recruitment proccess to Nokia</p>
     <h2>How to Use</h2>
     <ol>
         <li>Send a POST request to the following endpoints:</li>
         <ul>
-            <li><strong>/knn</strong> - For predictions using the KNN model.</li>
-            <li><strong>/gnb</strong> - For predictions using the Gaussian Naive Bayes model.</li>
-            <li><strong>/mlp</strong> - For predictions using the Multi-Layer Perceptron model.</li>
+            <li><strong>/knn</strong> - For predictions using the kNN model.</li>
+            <li><strong>/gnb</strong> - For predictions using the GNB model.</li>
+            <li><strong>/mlp</strong> - For predictions using the MLP model.</li>
         </ul>
-        <li>The POST request should contain JSON data in the following format (below is already sample data which you can send on the backend server):</li>
+        <li>The POST request should contain JSON data in the following format (below is already sample data which you can send on the backend server from postman or client.rest VSCode Extention):</li>
         <pre>{
     "features": [5.0, 13.0, 9.0, 1.0, 0.0, 0.0, 0.0, 13.0, 15.0, 10.0, 15.0, 5.0, 3.0, 15.0, 2.0, 0.0, 11.0, 8.0, 4.0, 12.0, 0.0, 0.0, 8.0, 8.0, 5.0, 8.0, 0.0, 0.0, 9.0, 8.0, 4.0, 11.0, 0.0, 1.0, 12.0, 7.0, 2.0, 14.0, 5.0, 10.0, 12.0, 0.0, 6.0, 13.0, 10.0, 0.0, 0.0, 0.0]
 }</pre>
         <li>You will receive a JSON response with the model's prediction.</li>
+        <pre>{
+    "Prediction": 0
+}</pre>
     </ol>
     <p>Checking the prediction using <strong>curl</strong>:</p>
     <span>curl -X POST https://nokia-jakubiwaszkeiwicz-recruitment-task.azurewebsites.net/knn -H "Content-Type: application/json" -d '{"features": [5.0, 13.0, 9.0, 1.0, 0.0, 0.0, 0.0, 13.0, 15.0, 10.0, 15.0, 5.0, 3.0, 15.0, 2.0, 0.0, 11.0, 8.0, 4.0, 12.0, 0.0, 0.0, 8.0, 8.0, 5.0, 8.0, 0.0, 0.0, 9.0, 8.0, 4.0, 11.0, 0.0, 1.0, 12.0, 7.0, 2.0, 14.0, 5.0, 10.0, 12.0, 0.0, 6.0, 13.0, 10.0, 0.0, 0.0, 0.0]}'</span>
